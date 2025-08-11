28a5bb8793e8607c8e0cfa47661e7d862a30687f
Maria <maru.27@gmail.com>
2025-08-11 11:48:30 -0400

    funciones de agrgar y sortear amigos

--- app.js
+++ app.js
@@ -3,14 +3,40 @@ let amigos = []
 
 function agregarAmigo() {
     const input = document.getElementById("amigo");
-    const nombre = input.value.trim(); // trim() para eliminar espacios extra
+    const nombre = input.value.trim();
 
     if (nombre === "") {
         alert("Por favor, inserte un nombre.");
         return;
     }
 
-    listaAmigos.push(nombre);
+    amigos.push(nombre); // usamos el array amigos
     mostrarLista();
     input.value = "";
+}
+
+function mostrarLista() {
+  const listaHTML = document.getElementById("listaAmigos");
+
+  listaHTML.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    listaHTML.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+    const listaHTML = document.getElementById("listaAmigos");
+    if (listaHTML === "") {
+        alert("No hay amigos para sortear");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultadoHTML = document.getElementById("resultado");
+    resultadoHTML.innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
 }
